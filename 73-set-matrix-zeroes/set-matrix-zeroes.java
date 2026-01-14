6d64da26b89e799bd555294c4012dbafbe81c5b6
shivprasad <151425596+shivprasad31@users.noreply.github.com>
2026-01-14 13:54:41 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (8.28%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,54 +1,25 @@
 class Solution {
-    boolean[] zeroRow;
-    boolean[] zeroCol;
-    boolean[][] visited;
-
     public void setZeroes(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-
-        zeroRow = new boolean[r];
-        zeroCol = new boolean[c];
-        visited = new boolean[r][c];
-
-        // discover all zero influence
+        int[] row=new int[matrix.length];
+        int[] col=new int[matrix[0].length];
+        int r=matrix.length;
+        int c=matrix[0].length;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(matrix[i][j]==0 && !visited[i][j]){
-                    helper(matrix, i, j);
+                if(matrix[i][j]==0){
+                    row[i]=-1;
+                    col[j]=-1;
                 }
             }
         }
 
-        // apply zeros
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(zeroRow[i] || zeroCol[j]){
-                    matrix[i][j] = 0;
+                if(row[i]==-1||col[j]==-1){
+                    matrix[i][j]=0;
                 }
             }
         }
-    }
-
-    void helper(int[][] mat, int r, int c){
-        if(visited[r][c]) return;
-        visited[r][c] = true;
-
-        zeroRow[r] = true;
-        zeroCol[c] = true;
-
-        // scan row
-        for(int j=0;j<mat[0].length;j++){
-            if(mat[r][j]==0){
-                helper(mat, r, j);
-            }
-        }
-
-        // scan column
-        for(int i=0;i<mat.length;i++){
-            if(mat[i][c]==0){
-                helper(mat, i, c);
-            }
-        }
+        
     }
 }
