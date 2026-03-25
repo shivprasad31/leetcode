00346929a995375fd0ceeb894191ce0474c2e09b
shivprasad <151425596+shivprasad31@users.noreply.github.com>
2026-03-25 09:39:20 +0530

    Time: 46 ms (14.29%) | Memory: 47.1 MB (67.59%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,24 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans=new int[2];
-        HashSet<Integer> set=new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-        }
-        for(int i=0;i<nums.length;i++){
-            int toFind=target-nums[i];
-            if(set.contains(toFind)){
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[j]==toFind){
-                        ans[0]=i;
-                        ans[1]=j;
-                        return ans;
-                    }
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
                 }
             }
-
-        }
-        return ans;
-        
+        }  
+        return ans;      
     }
 }
